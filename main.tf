13f63a77e9d5faf6112dd2275db6f54d3795f038
Julian Nonino <noninojulian@gmail.com>
2019-10-01 15:29:19 +0100

    Updating ECS Fargate Reference

--- main.tf
+++ main.tf
@@ -39,8 +39,8 @@ resource "aws_kms_key" "encryption_key" {
 # ECS Fargate Service
 # ---------------------------------------------------------------------------------------------------------------------
 module "ecs_fargate" {
-  source  = "jnonino/ecs-fargate/aws"
-  version = "2.0.1"
+  source  = "cn-terraform/ecs-fargate/aws"
+  version = "2.0.5"
   #source = "../terraform-aws-ecs-fargate"
 
   name_preffix                 = "${var.name_preffix}-sonar"
