cb7ac8479855473c0efbb7d700f0c6c561644f10
Julian Nonino <noninojulian@gmail.com>
2020-05-02 17:47:42 +0100

    Updating base module

--- main.tf
+++ main.tf
@@ -20,42 +20,33 @@ locals {
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
-# ECS Cluster
-# ---------------------------------------------------------------------------------------------------------------------
-resource "aws_ecs_cluster" "sonar_cluster" {
-  name = "${var.name_preffix}-sonar"
-}
-
-# ---------------------------------------------------------------------------------------------------------------------
-# AWS KMS Encryption Key
-# ---------------------------------------------------------------------------------------------------------------------
-resource "aws_kms_key" "encryption_key" {
-  description         = "Sonar Encryption Key"
-  is_enabled          = true
-  enable_key_rotation = true
-}
-
-# ---------------------------------------------------------------------------------------------------------------------
 # ECS Fargate Service
 # ---------------------------------------------------------------------------------------------------------------------
 module "ecs_fargate" {
-  source  = "cn-terraform/ecs-fargate/aws"
-  version = "2.0.9"
-  #source = "../terraform-aws-ecs-fargate"
+  # source  = "cn-terraform/ecs-fargate/aws"
+  # version = "2.0.12"
+  source = "../terraform-aws-ecs-fargate"
 
   name_preffix                 = "${var.name_preffix}-sonar"
   profile                      = var.profile
   region                       = var.region
+
   vpc_id                       = var.vpc_id
-  availability_zones           = var.availability_zones
   public_subnets_ids           = var.public_subnets_ids
   private_subnets_ids          = var.private_subnets_ids
   container_name               = "${var.name_preffix}-sonar"
-  container_image              = "sonarqube:lts"
+  container_image              = "cnservices/sonarqube"
   container_cpu                = 1024
   container_memory             = 8192
-  container_memory_reservation = 2048
-  container_port               = 9000
+  lb_http_ports                = [ 9000 ]
+  lb_https_ports               = []
+  port_mappings = [
+    {
+      containerPort = 9000
+      hostPort      = 9000
+      protocol      = "tcp"
+    }
+  ]
   environment = [
     {
       name  = "sonar.jdbc.username"
