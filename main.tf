0ea493381b17c174003df12e7c3f266c7df76199
Julian Nonino <noninojulian@gmail.com>
2020-05-27 10:09:55 +0100

    Updating module dependency

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ module aws_cw_logs {
 #------------------------------------------------------------------------------
 module "ecs_fargate" {
   source  = "cn-terraform/ecs-fargate/aws"
-  version = "2.0.15"
+  version = "2.0.16"
   # source = "../terraform-aws-ecs-fargate"
 
   name_preffix                 = "${var.name_preffix}-sonar"
