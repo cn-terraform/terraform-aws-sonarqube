20c2451861990a0dd9f69cc5622f23ac3038775c
Julian Nonino <jnonino@users.noreply.github.com>
2022-02-11 20:06:18 +0000

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -3,10 +3,12 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "3.74.1"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
+      version = "3.1.0"
     }
   }
 }
