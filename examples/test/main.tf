e7ed69382194521dd072f2a80068b5c690f73eef
Julian Nonino <jnonino@users.noreply.github.com>
2025-03-27 23:11:32 +0000

    Update networking module in example

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -1,12 +1,46 @@
 module "base-network" {
-  source                                      = "cn-terraform/networking/aws"
+  source  = "cn-terraform/networking/aws"
   version = "2.0.16"
+  
+  cidr_block = "192.168.0.0/16"
 
-  name_prefix                                 = "test-networking"
-  vpc_cidr_block                              = "192.168.0.0/16"
-  availability_zones                          = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
-  public_subnets_cidrs_per_availability_zone  = ["192.168.0.0/19", "192.168.32.0/19", "192.168.64.0/19", "192.168.96.0/19"]
-  private_subnets_cidrs_per_availability_zone = ["192.168.128.0/19", "192.168.160.0/19", "192.168.192.0/19", "192.168.224.0/19"]
+  public_subnets = {
+    first_public_subnet = {
+      availability_zone = "us-east-1a"
+      cidr_block        = "192.168.0.0/19"
+    }
+    second_public_subnet = {
+      availability_zone = "us-east-1b"
+      cidr_block        = "192.168.32.0/19"
+    }
+  third_public_subnet = {
+      availability_zone = "us-east-1c"
+      cidr_block        = "192.168.64.0/19"
+    }
+  fourth_public_subnet = {
+      availability_zone = "us-east-1d"
+      cidr_block        = "192.168.96.0/19"
+    }
+  }
+
+  private_subnets = {
+    first_private_subnet = {
+      availability_zone = "us-east-1a"
+      cidr_block        = "192.168.128.0/19"
+    }
+    second_private_subnet = {
+      availability_zone = "us-east-1b"
+      cidr_block        = "192.168.160.0/19"
+    }
+    third_private_subnet = {
+      availability_zone = "us-east-1c"
+      cidr_block        = "192.168.192.0/19"
+    }
+    fourth_private_subnet = {
+      availability_zone = "us-east-1d"
+      cidr_block        = "192.168.224.0/19"
+    }
+  }
 }
 
 module "sonar" {
